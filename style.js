a6fe21ffddcab9b18d9b428a4357168a75cf60fe
davefowler3 <davejf3@gmail.com>
2021-06-15 11:38:10 -0400

    Working on nav responsiveness.

--- style.js
+++ style.js
@@ -1,28 +1,8 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_links');
-    const navLinks = document.querySelectorAll('.nav_links li');
+const hamburgerButton = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation ) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
-            }
-        });
-
-        // Burger Animation
-        burger.classList.toggle('toggle');
-    });
+function toggleButton() {
+    navList.classList.toggle("show");
 }
 
-// Use this type of syntax if you're going to call multiple functions. 
-const app = ()=> {
-    navSlide();
-}
-
-app();
+hamburgerButton.addEventListener("click", toggleButton);
