f25672f7e9d258319a3d41d59bdae6a515de1675
davefowler3 <davejf3@gmail.com>
2021-06-25 08:20:14 -0400

    AMobile responsiveness.

--- style.js
+++ style.js
@@ -6,3 +6,16 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener("click", toggleButton);
+
+
+
+// [START maps_map_simple]
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+// [END maps_map_simple]
