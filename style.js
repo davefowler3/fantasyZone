406302b9e59d2a22c427037c4f90ff5dbbab84e5
davefowler3 <davejf3@gmail.com>
2021-06-25 10:52:07 -0400

    Adjusted about section bottom padding.

--- style.js
+++ style.js
@@ -9,13 +9,3 @@ hamburgerButton.addEventListener("click", toggleButton);
 
 
 
-// [START maps_map_simple]
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-// [END maps_map_simple]
